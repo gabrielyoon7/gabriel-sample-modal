ce415363047bf6ce30b863f11a5d213af509050f
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-02 17:37:11 +0900

    fix: 배포 버그 수정

--- src/lib/Modal.tsx
+++ src/lib/Modal.tsx
@@ -1,5 +1,34 @@
 import React, { useState } from "react";
-import "./styles.css";
+import styled from 'styled-components';
+
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Modal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+  width: 500px; /* 수정 */
+  height: 500px; /* 수정 */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`
 
 const MyModal = ({ trigger, children }: { trigger: any, children: any }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -16,11 +45,11 @@ const MyModal = ({ trigger, children }: { trigger: any, children: any }) => {
     <>
       {React.cloneElement(trigger, { onClick: handleOpenModal })}
       {isModalOpen && (
-        <div className="modal-overlay" onClick={handleCloseModal}>
-          <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <ModalOverlay onClick={handleCloseModal}>
+          <Modal onClick={(e) => e.stopPropagation()}>
             {children}
-          </div>
-        </div>
+          </Modal>
+        </ModalOverlay>
       )}
     </>
   );
